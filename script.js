77aa1545c29e86b3ab5fbe3edbba4c4f3b7e3c35
RajatBhullar <rajatsinghrs1801@gmail.com>
2019-10-17 16:32:38 +0530

    My very first commit

---
+++ script.js
@@ -0,0 +1,117 @@
+$(document).ready(function(){
+    $("#button1").click(function(){
+        $("#div1").show()
+        $("#div2").hide()
+        $("#div3").hide()
+        $("#div4").hide()
+        $("#div5").hide()
+        $("#div6").hide()
+        $("#div7").hide()
+        $("#div8").hide()
+        $("#h3").hide()
+
+    })
+})
+
+$(document).ready(function(){
+    $("#button2").click(function(){
+        $("#div2").show()
+        $("#div1").hide()
+        $("#div3").hide()
+        $("#div4").hide()
+        $("#div5").hide()
+        $("#div6").hide()
+        $("#div7").hide()
+        $("#div8").hide()
+        $("#h3").hide()
+    })
+})
+
+$(document).ready(function(){
+    $("#button3").click(function(){
+        $("#div3").show()
+        $("#div1").hide()
+        $("#div2").hide()
+        $("#div4").hide()
+        $("#div5").hide()
+        $("#div6").hide()
+        $("#div7").hide()
+        $("#div8").hide()
+        $("#h3").hide()
+
+    })
+})
+
+$(document).ready(function(){
+    $("#button4").click(function(){
+        $("#div4").show()
+        $("#div1").hide()
+        $("#div3").hide()
+        $("#div2").hide()
+        $("#div5").hide()
+        $("#div6").hide()
+        $("#div7").hide()
+        $("#div8").hide()
+        $("#h3").hide()
+
+    })
+})
+
+$(document).ready(function(){
+    $("#button5").click(function(){
+        $("#div5").show()
+        $("#div1").hide()
+        $("#div3").hide()
+        $("#div4").hide()
+        $("#div2").hide()
+        $("#div6").hide()
+        $("#div7").hide()
+        $("#div8").hide()
+        $("#h3").hide()
+
+    })
+})
+
+$(document).ready(function(){
+    $("#button6").click(function(){
+        $("#div6").show()
+        $("#div1").hide()
+        $("#div3").hide()
+        $("#div4").hide()
+        $("#div5").hide()
+        $("#div2").hide()
+        $("#div7").hide()
+        $("#div8").hide()
+        $("#h3").hide()
+
+    })
+})
+
+$(document).ready(function(){
+    $("#button7").click(function(){
+        $("#div7").show()
+        $("#div1").hide()
+        $("#div3").hide()
+        $("#div4").hide()
+        $("#div5").hide()
+        $("#div6").hide()
+        $("#div2").hide()
+        $("#div8").hide()
+
+    })
+})
+
+$(document).ready(function(){
+    $("#button8").click(function(){
+        $("#div8").show()
+        $("#div1").hide()
+        $("#div3").hide()
+        $("#div4").hide()
+        $("#div5").hide()
+        $("#div6").hide()
+        $("#div7").hide()
+        $("#div2").hide()
+
+    })
+})
+
